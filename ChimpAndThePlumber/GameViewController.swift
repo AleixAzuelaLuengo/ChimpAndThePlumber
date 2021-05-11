351a11f3a87e698a3b8385ef3bcd83be400fb1d4
Aleix <aleix.azuela@enti.cat>
2021-05-11 23:01:47 +0200

    Map Done

--- ChimpAndThePlumber/GameViewController.swift
+++ ChimpAndThePlumber/GameViewController.swift
@@ -8,36 +8,35 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import SKTiled
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
         if let scene = GKScene(fileNamed: "GameScene") {
-            
             // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+            guard let sceneNode = scene.rootNode as? GameScene else { return }
+            
+            // Copy gameplay related content over to the scene
+            sceneNode.entities = scene.entities
+            sceneNode.graphs = scene.graphs
+            
+            // Set the scale mode to scale to fit the window
+            sceneNode.scaleMode = .aspectFill
+            
+            // Present the scene
+            guard let view = self.view as? SKView else { return }
+            
+            view.presentScene(sceneNode)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
         }
     }
 
---
+++ Pods/Target Support Files/Pods-ChimpAndThePlumber/Pods-ChimpAndThePlumber-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChimpAndThePlumber : NSObject
+@end
+@implementation PodsDummy_Pods_ChimpAndThePlumber
+@end
